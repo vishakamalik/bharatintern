59cca51f460d98328048d10fc0a84e83f74410da
vishakamalik <vishakamalik13@gmail.com>
2024-03-31 19:24:17 +0530

    First commit

---
+++ portfolio/popup.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-pop-up");
+}
+
+function closePopup(){
+    popup.classList.remove("open-pop-up");
+}
